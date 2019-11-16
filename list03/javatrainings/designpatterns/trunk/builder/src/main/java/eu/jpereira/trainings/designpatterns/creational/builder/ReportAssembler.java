e8ec1600ae5be324508ccc3ecc2ac52d300357da
Piotr Popis <piotrpopis98@icloud.com>
2019-11-16 16:17:58 +0100

    builder almofinished

--- list03/javatrainings/designpatterns/trunk/builder/src/main/java/eu/jpereira/trainings/designpatterns/creational/builder/ReportAssembler.java
+++ list03/javatrainings/designpatterns/trunk/builder/src/main/java/eu/jpereira/trainings/designpatterns/creational/builder/ReportAssembler.java
@@ -1,17 +1,17 @@
 /**
  * Copyright 2011 Joao Miguel Pereira
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package eu.jpereira.trainings.designpatterns.creational.builder;
 
@@ -19,33 +19,34 @@ import java.util.Iterator;
 
 import eu.jpereira.trainings.designpatterns.creational.builder.json.JSONReportBody;
 import eu.jpereira.trainings.designpatterns.creational.builder.model.Report;
+import eu.jpereira.trainings.designpatterns.creational.builder.model.ReportBodyBuilder;
 import eu.jpereira.trainings.designpatterns.creational.builder.model.SaleEntry;
 import eu.jpereira.trainings.designpatterns.creational.builder.model.SoldItem;
 import eu.jpereira.trainings.designpatterns.creational.builder.xml.XMLReportBody;
 
 /**
  * @author jpereira
- * 
+ *
  */
 public class ReportAssembler {
 
-	private SaleEntry saleEntry;
+    private SaleEntry saleEntry;
 
-	/**
-	 * @param
-	 */
-	public void setSaleEntry(SaleEntry saleEntry) {
-		this.saleEntry = saleEntry;
+    /**
+     * @param
+     */
+    public void setSaleEntry(SaleEntry saleEntry) {
+        this.saleEntry = saleEntry;
 
-	}
+    }
 
-	/**
-	 * @param type
-	 * @return
-	 */
-	public Report getReport(String type) {
-		Report report = new Report();
-
+    public Report getReport(ReportBodyBuilder reportBodyBuilder) {
+        Report report = new Report();
+        reportBodyBuilder.setCustomerName(saleEntry.getCustomer().getName());
+        reportBodyBuilder.withItems();
+        Iterator<SoldItem> iterator = saleEntry.getSoldItems().iterator()
+    }
+/*
 		// Algorithms to build the body objects are different
 		if (type.equals("JSON")) {
 
@@ -129,6 +130,6 @@ public class ReportAssembler {
 		}
 
 		return report;
-	}
+	}*/
 
 }
--- list03/javatrainings/designpatterns/trunk/builder/src/main/java/eu/jpereira/trainings/designpatterns/creational/builder/model/HTMLReportBodyBuilder.java
+++ list03/javatrainings/designpatterns/trunk/builder/src/main/java/eu/jpereira/trainings/designpatterns/creational/builder/model/HTMLReportBodyBuilder.java
@@ -1,4 +1,51 @@
 package eu.jpereira.trainings.designpatterns.creational.builder.model;
 
-public class HTMLReportBodyBuilder {
+import eu.jpereira.trainings.designpatterns.creational.builder.HTMLReportBody;
+
+public class HTMLReportBodyBuilder extends HTMLReportBody implements ReportBodyBuilder {
+    private HTMLReportBody reportBody;
+
+    public HTMLReportBodyBuilder() {
+        reportBody = new HTMLReportBody();
+    }
+
+    @Override
+    public ReportBodyBuilder setCustomerName(String customerName) {
+        reportBody.putContent("<span class=\"customerName\">");
+        reportBody.putContent(customerName);
+        reportBody.putContent("</span>");
+        return this;
+    }
+
+    @Override
+    public ReportBodyBuilder setCustomerPhone(String phoneNumber) {
+        reportBody.putContent("<span class=\"customerPhone\">");
+        reportBody.putContent(phoneNumber);
+        reportBody.putContent("</span>");
+        return this;
+    }
+
+    @Override
+    public ReportBodyBuilder withItems() {
+        reportBody.putContent("<items>");
+        return this;
+    }
+
+    @Override
+    public ReportBodyBuilder newItem(String name, int quantity, double price) {
+        reportBody.putContent("<item><name>");
+        reportBody.putContent(name);
+        reportBody.putContent("</name><quantity>");
+        reportBody.putContent(quantity);
+        reportBody.putContent("</quantity><price>");
+        reportBody.putContent(price);
+        reportBody.putContent("</price></item>");
+        return this;
+    }
+
+    @Override
+    public ReportBody getReportBody() {
+        reportBody.putContent("</items>");
+        return reportBody;
+    }
 }
